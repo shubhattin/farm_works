b4c75e12ad199891a819cc60fe0a4af1e28c1c26
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-01-17 00:00:05 +0530

    test

--- src/components/pages/vistrita/AddRecord.svelte
+++ src/components/pages/vistrita/AddRecord.svelte
@@ -192,7 +192,13 @@
           देयक जोड़ें
         </button>
       {/if}
-      <DateInput bind:value={date} />
+      <DateInput
+        bind:value={date}
+        required={true}
+        placeholder="दिनांक"
+        format="dd-MM-yy HH:mm"
+        timePrecision={'minute'}
+      />
     </div>
   </form>
 {:else}
