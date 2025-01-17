b54688821123bec2c047cb6dc66c95948b5d2e33
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-01-17 22:39:19 +0530

    asd

--- src/db/schema_zod.ts
+++ src/db/schema_zod.ts
@@ -13,13 +13,15 @@ import { createSelectSchema } from 'drizzle-zod';
 export const UsersSchemaZod = createSelectSchema(users);
 export const CustomersSchemaZod = createSelectSchema(customers);
 export const BillsSchemaZod = createSelectSchema(bills, {
-  timestamp: z.coerce.date()
+  timestamp: z.coerce.date(),
+  date: z.coerce.date()
 });
 export const KaTAIRecordsSchemaZod = createSelectSchema(kaTAI_records);
 export const JotAIRecordsSchemaZod = createSelectSchema(jotAI_records);
 export const TrolleyRecordsSchemaZod = createSelectSchema(trolley_records);
 export const PaymentsSchemaZod = createSelectSchema(payments, {
-  timestamp: z.coerce.date()
+  timestamp: z.coerce.date(),
+  date: z.coerce.date()
 });
 
 // enums
