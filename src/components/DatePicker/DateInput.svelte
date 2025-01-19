88f1e3ea5c3e00622b0cab6cee6e53a8b11fbd3b
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-01-19 12:13:33 +0530

    fix date component

--- src/components/DatePicker/DateInput.svelte
+++ src/components/DatePicker/DateInput.svelte
@@ -161,7 +161,7 @@
 <div class="relative {classes}" on:focusout={onFocusOut} on:keydown={keydown}>
   <input
     bind:this={InputElement}
-    class="input m-0 box-border w-[150px] min-w-0 rounded-lg
+    class="input m-0 box-border w-[150px] min-w-0 rounded-xl
            border border-gray-300/30
            px-2 py-1 outline-none
            transition-all duration-75 ease-in-out
